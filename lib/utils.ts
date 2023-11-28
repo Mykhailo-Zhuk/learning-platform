48b0e29923ee85b43fe144326440e854ebf71982
Mykhailo-Zhuk <mishazhuk1@gmail.com>
2023-11-28 22:23:38 +0200

    some in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,31 +1,11 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const baseURL = "http://localhost:3000/" // "https://learning-platform-dusky.vercel.app/"; //"http://localhost:3000/";
+export const baseURL = "https://learning-platform-dusky.vercel.app/"; //"http://localhost:3000/";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const fetchDataFromKV = (
-  endpoint: string,
-  method = "get",
-  data?: { id: string | number; name: string; password: string; role: "admin" | "student" },
-) => {
-  return fetch(
-    `https://faithful-roughy-43695.kv.vercel-storage.com/json.${
-      method !== "get" ? "set" : "get"
-    }/${endpoint}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.kv_REST_API_TOKEN}`,
-      },
-      method,
-      body: JSON.stringify(data) ?? null,
-    },
-  );
-};
-
 export const fetchDataFromNextServer = (
   endpoint: string,
   method = "get",
